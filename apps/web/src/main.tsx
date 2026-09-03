5f5716cb19c642bca2d657b2f26d608e37e28322
Vishal Kumar <257167228+vishaltradewinds@users.noreply.github.com>
2026-09-03 14:37:30 +0530

    Show secure login screen before RupayKG workspace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import LoginGate from "./LoginGate";
 import "./styles.css";
+
+function Root() {
+  const [authenticated, setAuthenticated] = useState(() => Boolean(sessionStorage.getItem("rupaykg.sessionToken")));
+  return authenticated ? <App /> : <LoginGate onAuthenticated={() => setAuthenticated(true)} />;
+}
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
